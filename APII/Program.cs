8e35b7f8e0e2c68f3d93b1aae7abdc656d43667b
Joeykwes <joey.kwestro@kensan.nl>
2023-10-09 09:33:28 +0200

    API can receive Payload from http POST request

--- APII/Program.cs
+++ APII/Program.cs
@@ -1,29 +1,50 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
+﻿using System;
+using System.IO;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var host = new WebHostBuilder()
-            .UseKestrel()
-            .ConfigureServices(services =>
-            {
-                services.AddControllers();
-            })
-            .Configure(app =>
-            {
-                app.UseRouting();
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                });
-            })
-            .Build();
+        // Create an HttpListener instance to listen for incoming requests
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8080/"); // Define the URL to listen on
 
-        host.Run();
+        try
+        {
+            listener.Start();
+            Console.WriteLine("Listening for requests on http://localhost:8080/");
+
+            while (true)
+            {
+                // Wait for an incoming HTTP request
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+
+                // Read the request body
+                string requestBody;
+                using (StreamReader reader = new StreamReader(request.InputStream))
+                {
+                    requestBody = reader.ReadToEnd();
+                }
+
+                // Print the request body
+                Console.WriteLine($"Received a request with body: {requestBody}");
+
+                // Send a response to the client
+                string responseText = "Hello, this is your API response!";
+                byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(responseText);
+                context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+                context.Response.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        finally
+        {
+            listener.Stop();
+        }
     }
 }
