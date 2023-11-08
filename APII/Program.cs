d8efe4a01392944d7033de57c955ee4e72e2dc51
DYWerensteijn <99057620@mydavinci.nl>
2023-11-08 15:28:27 +0100

    reading specific payload data. still has errors

--- APII/Program.cs
+++ APII/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
 using System.Net;
+using System.Text;
+using System.Text.Json;
 
 class Program
 {
@@ -29,11 +32,27 @@ class Program
                 }
 
                 // Print the request body
-                Console.WriteLine($"Received a request with body: {requestBody}");
+                // Console.WriteLine($"Received a request with body: {requestBody}");
 
                 // Send a response to the client
-                string responseText = "Hello, this is your API response!";
-                byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(responseText);
+                
+                byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(requestBody);
+                string resultString = Encoding.UTF8.GetString(responseBytes);
+
+                List<RootObject> responseJSON = JsonSerializer.Deserialize<List<RootObject>>(resultString);
+
+                if (responseJSON != null && responseJSON.Count > 0 &&
+    responseJSON[0].UplinkMetaData != null && responseJSON[0].UplinkMetaData.RxInfo != null &&
+    responseJSON[0].UplinkMetaData.RxInfo.Count > 0)
+                {
+                    string gatewayID = responseJSON[0].UplinkMetaData.RxInfo[0].GatewayID;
+                    string devAddr = responseJSON[0].PhyPayload.MacPayload.Fhdr.DevAddr;
+
+                    Console.WriteLine($"Result of testy test: {responseJSON}");
+
+                    Console.WriteLine("Gateway ID: " + gatewayID);
+                    Console.WriteLine("DevAddr: " + devAddr);
+                }
                 context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
                 context.Response.Close();
             }
--- APII/Read_Payload.cs
+++ APII/Read_Payload.cs
@@ -1,4 +1,6 @@
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 public class Location
 {
@@ -27,33 +29,92 @@ public class Root
     public string crcStatus { get; set; }
 }
 
-string json = @"{
+public class RxInfo
 {
-    ""gatewayID"": ""AAAAAAAAACI="",
-    ""time"": ""2023-10-03T11:22:11.178121Z"",
-    ""timeSinceGPSEpoch"": null,
-    ""rssi"": -116,
-    ""loRaSNR"": -5.2,
-    ""channel"": 0,
-    ""rfChain"": 1,
-    ""board"": 0,
-    ""antenna"": 0,
-    ""location"": {
-        ""latitude"": 51.81031,
-        ""longitude"": 4.62213,
-        ""altitude"": 9,
-        ""source"": ""UNKNOWN"",
-        ""accuracy"": 0
-    },
-    ""fineTimestampType"": ""NONE"",
-    ""context"": ""DQaIyw=="",
-    ""uplinkID"": ""k0OrDvjtSQGQN/eQ10be2Q=="",
-    ""crcStatus"": ""CRC_OK""
+    public string GatewayID { get; set; }
+    public object Time { get; set; }
+    public string TimeSinceGPSEpoch { get; set; }
+    public int Rssi { get; set; }
+    public int LoRaSNR { get; set; }
+    public int Channel { get; set; }
+    public int RfChain { get; set; }
+    public int Board
+    {
+        get; set;
+    }
+    public int Antenna { get; set; }
+    public Location Location { get; set; }
+    public string FineTimestampType { get; set; }
+    public string Context { get; set; }
+    public string UplinkID { get; set; }
+    public string CrcStatus { get; set; }
 }
 
-    // hier komt de ontvangen payload.
-}";
+public class LoRaModulationInfo
+{
+    public int Bandwidth { get; set; }
+    public int SpreadingFactor { get; set; }
+    public string CodeRate { get; set; }
+    public bool PolarizationInversion { get; set; }
+}
 
-Root root = JsonConvert.DeserializeObject<Root>(json);
+public class TxInfo
+{
+    public int Frequency { get; set; }
+    public string Modulation { get; set; }
+    public LoRaModulationInfo LoRaModulationInfo { get; set; }
+}
 
-Console.WriteLine($"Gateway ID: {root.gatewayID}");
+public class FCtrl
+{
+    public bool Adr { get; set; }
+    public bool AdrAckReq { get; set; }
+    public bool Ack { get; set; }
+    public bool FPending { get; set; }
+    public bool ClassB { get; set; }
+}
+
+public class Fhdr
+{
+    public string DevAddr { get; set; }
+    public FCtrl FCtrl { get; set; }
+    public int FCnt { get; set; }
+    public object FOpts { get; set; }
+}
+
+public class FrmPayload
+{
+    public string Bytes { get; set; }
+}
+
+public class MacPayload
+{
+    public Fhdr Fhdr { get; set; }
+    public int FPort { get; set; }
+    public List<FrmPayload> FrmPayload { get; set; }
+    public string Mic { get; set; }
+}
+
+public class Mhdr
+{
+    public string MType { get; set; }
+    public string Major { get; set; }
+}
+
+public class PhyPayload
+{
+    public Mhdr Mhdr { get; set; }
+    public MacPayload MacPayload { get; set; }
+}
+
+public class UplinkMetaData
+{
+    public List<RxInfo> RxInfo { get; set; }
+    public TxInfo TxInfo { get; set; }
+}
+
+public class RootObject
+{
+    public UplinkMetaData UplinkMetaData { get; set; }
+    public PhyPayload PhyPayload { get; set; }
+}
