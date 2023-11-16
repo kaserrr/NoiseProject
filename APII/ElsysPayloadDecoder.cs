7388aa388b08d212f90d21272c2a5236587060d4
StefanNijssen <stefan.nijssen68@gmail.com>
2023-11-16 10:17:17 +0100

    begin gemaakt aan Smart decoder

--- APII/ElsysPayloadDecoder.cs
+++ APII/ElsysPayloadDecoder.cs
@@ -230,11 +230,12 @@ public class ElsysPayloadDecoder
     }
 }
 
+
 class Decoder
 {
     static void Main(string[] args)
     {
-        string data = "0100e202290400270506060308070d62"; // Replace with your hexadecimal data
+        string data = "447046e66e5359b1838cb674c8713e03"; // Replace with your hexadecimal data
         Dictionary<string, object> result = ElsysPayloadDecoder.DecodeElsysPayload(data);
 
         foreach (var item in result)
---
+++ APII/FLSmartPayloadDecoder.cs
@@ -0,0 +1,65 @@
+﻿using Syusing System;
+using System.Collections.Generic;
+
+class DecodFLSmartPayloadDecoderer
+{
+    static void Main()
+    {
+        // Example payload bytes (replace this with your actual payload data)
+        string payloadHex = "0A00003C0005000000000000140A0F37";
+        byte[] payloadBytes = StringToByteArray(payloadHex);
+
+        Dictionary<string, object> decodedData = DecodeFLSmartOfficeLoRaPayload(payloadBytes);
+
+        foreach (var kvp in decodedData)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+
+    static Dictionary<string, object> DecodeFLSmartOfficeLoRaPayload(byte[] data)
+    {
+        if (data.Length != 16)
+        {
+            return new Dictionary<string, object> { { "Error", "Invalid payload length" } };
+        }
+
+        Dictionary<string, object> decodedData = new Dictionary<string, object>();
+
+        // Extracting data bytes
+        byte attempts = data[0];
+        ushort pending = BitConverter.ToUInt16(data, 1);
+        ushort batteryVoltage = BitConverter.ToUInt16(data, 3);
+        float batteryVoltageValue = batteryVoltage * 0.01f;
+        ushort co2Level = BitConverter.ToUInt16(data, 5);
+        float temperature = BitConverter.ToUInt16(data, 7) * 0.01f;
+        uint humidity = BitConverter.ToUInt32(data, 9);
+        float humidityValue = humidity * 0.1f;
+        byte lightIntensity = data[13];
+        byte soundMax = data[14];
+        byte soundAvg = data[15];
+
+        decodedData.Add("Attempts", attempts);
+        decodedData.Add("Pending", pending);
+        decodedData.Add("Battery Voltage (V)", batteryVoltageValue);
+        decodedData.Add("CO2 Level (ppm)", co2Level);
+        decodedData.Add("Temperature (K)", temperature);
+        decodedData.Add("Humidity (%)", humidityValue);
+        decodedData.Add("Light Intensity (Lux)", lightIntensity);
+        decodedData.Add("Sound Maximum (dB(A))", soundMax);
+        decodedData.Add("Sound Average (dB(A))", soundAvg);
+
+        return decodedData;
+    }
+
+    static byte[] StringToByteArray(string hex)
+    {
+        int numberChars = hex.Length;
+        byte[] bytes = new byte[numberChars / 2];
+        for (int i = 0; i < numberChars; i += 2)
+        {
+            bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+        }
+        return bytes;
+    }
+}
